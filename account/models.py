c2c230e0f3b895eea92fdbb1241c5d5d974de2b4
卜星星 <boxingxing@limikeji.com>
2020-10-20 15:07:35 +0800

    角色与部门

--- account/models.py
+++ account/models.py
@@ -16,8 +16,8 @@ class UserModel(BaseModel):
         (TYP_LDAP, 'LDAP用户'),
     )
 
-    ST_NORMAL = 1
-    ST_FORBIDDEN = 2
+    ST_NORMAL = 10
+    ST_FORBIDDEN = 20
     ST_CHOICES = (
         (ST_NORMAL, '正常'),
         (ST_FORBIDDEN, '禁用'),
@@ -62,3 +62,62 @@ class UserModel(BaseModel):
         signer = TimestampSigner()
         token = signer.sign(self.id)
         return token
+
+
+class RoleModel(BaseModel):
+    '''
+    角色表
+    '''
+    TYP_SYSTEM = 10
+    TYP_NORMAL = 20
+    TYP_CHOICES = (
+        (TYP_SYSTEM, '系统角色'),
+        (TYP_NORMAL, '普通角色'),
+    )
+
+    name = models.CharField('角色名', max_length=128)
+    typ = models.IntegerField('类型', choices=TYP_CHOICES, default=TYP_NORMAL)
+
+    class Meta:
+        db_table = 'role'
+
+
+class RoleUserModel(BaseModel):
+    '''
+    角色用户关系表
+    '''
+    role = models.ForeignKey(RoleModel, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'role_user'
+
+
+class DepartmentModel(BaseModel):
+    '''
+    部门
+    '''
+    name = models.CharField('名称', max_length=128)
+    sign = models.CharField('标识', max_length=128)
+
+    class Meta:
+        db_table = 'department'
+
+
+class DepartmentUserModel(BaseModel):
+    '''
+    部门与用户
+    '''
+    TYP_MANAGER = 10
+    TYP_MEMBER = 20
+    TYP_CHOICES = (
+        (TYP_MANAGER, '部门负责人'),
+        (TYP_MEMBER, '普通成员'),
+    )
+
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    department = models.ForeignKey(DepartmentModel, on_delete=models.CASCADE)
+    typ = models.SmallIntegerField('类型', choices=TYP_CHOICES, default=TYP_MEMBER)
+
+    class Meta:
+        db_table = 'department_user'
