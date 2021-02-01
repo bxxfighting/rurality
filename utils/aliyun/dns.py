d3c68dc8719a9bf01397cbc910c0ae373a5e7d9d
卜星星 <boxingxing@limikeji.com>
2021-02-01 15:31:38 +0800

    增删解析记录

--- utils/aliyun/dns.py
+++ utils/aliyun/dns.py
@@ -1,6 +1,8 @@
 from aliyunsdkalidns.request.v20150109.DescribeDomainsRequest import DescribeDomainsRequest
 from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
 from aliyunsdkalidns.request.v20150109.DescribeDomainRecordInfoRequest import DescribeDomainRecordInfoRequest
+from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import AddDomainRecordRequest
+from aliyunsdkalidns.request.v20150109.DeleteDomainRecordRequest import DeleteDomainRecordRequest
 
 from .base import AliyunCli
 
@@ -57,3 +59,28 @@ class AliyunDNS(AliyunCli):
         request.set_RecordId(instance_id)
         data = self._request(request)
         return data
+
+    def create_domain_record(self, domain_name, rr, typ, value):
+        '''
+        添加域名解析记录
+        '''
+        request = AddDomainRecordRequest()
+        request.set_accept_format('json')
+        request.set_DomainName(domain_name)
+        request.set_RR(rr)
+        request.set_Type(typ)
+        request.set_Value(value)
+        data = self._request(request)
+        record_id = data.get('RecordId')
+        return record_id
+
+    def delete_domain_record(self, instance_id):
+        '''
+        删除域名解析记录
+        '''
+        request = DeleteDomainRecordRequest()
+        request.set_accept_format('json')
+        request.set_RecordId(instance_id)
+        data = self._request(request)
+        record_id = data.get('RecordId')
+        return record_id
